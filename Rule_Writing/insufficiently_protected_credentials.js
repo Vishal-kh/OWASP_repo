7e34ca58b590ba202c5229bc3b09bc690b13b9af
Vishal Kharche <vishal_kharche@persistent.com>
2024-08-27 14:41:54 +0530

    make corrections in example

--- Rule_Writing/insufficiently_protected_credentials.js
+++ Rule_Writing/insufficiently_protected_credentials.js
@@ -1,7 +1,3 @@
-/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
 var express = require('express')
 var loginController = express.Router()
 var jwt = require("jsonwebtoken")
